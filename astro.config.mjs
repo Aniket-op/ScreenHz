2c18ce30dbfa1db65888136b3fa97640e8f4404c
Aniket-op <ak.aniket807@gmail.com>
2026-06-24 21:18:42 +0530

    patch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://screenhz.app',
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [sitemap(), react()],
   vite: {
     build: {
